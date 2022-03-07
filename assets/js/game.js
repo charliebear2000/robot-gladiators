b9cb533753954affc5f56092aee298f2ee7dca26
Leah <amelia_rawls@ccpsnet.net>
2022-03-07 15:28:12 -0500

    initial game setup

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,6 +1,7 @@
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
+var playerMoney = 10
 
 console.log(playerName, playerAttack, playerHealth);
 
@@ -13,6 +14,12 @@ var fight = function() {
    // Alert players that they are starting the round
    window.alert("Welcome to Robot Gladiators!");
 
+   // ask players if they want to fight or skip
+   var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
+
+   //if player chooses to fight, then fight
+   if (promptFight ==="fight" || promptFight === "FIGHT") {
+      
    //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
    enemyHealth = enemyHealth - playerAttack;
 
@@ -32,13 +39,35 @@ var fight = function() {
 
   // Log a resulting message to the console so we know that it worked.
   console.log(enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining.");
-}
 
 // check player's health
-if (playerHealth <=0) {
+if (playerHealth <= 0) {
    window.alert(playerName + " has dies!");
 }
 else { 
-   window.alert(playerName + " still has " + playerHealth + " health left,");
+   window.alert(playerName + " still has " + playerHealth + " health left.");
 }
+
+// if player chooses to skip
+} else if (promptFight === "skip" || promptFight === "SKIP") {
+      // confirm player wants to skip
+   var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+
+   // if yes (true), leave fight
+   if (confirmSkip) {
+      window.alert(playerName + " has decided to skip the fight. Goodbye!");
+      // subtract money from playerMoney for skipping
+      playerMoney = playerMoney - 2;
+      } 
+   // if no (false), ask question again by running fight() again
+   else {
+      fight();
+   }
+   // if player did not choose 1 or 2 in prompt
+   } else {
+      window.alert("You need to choose a valid option. Try again!");
+   }
+};
+
+// run fight function to start game
 fight();
